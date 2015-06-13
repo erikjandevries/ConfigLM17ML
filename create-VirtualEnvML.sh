07960a2950e1bce7ffea01534f164a853596bf1c
Erik Jan de Vries <erikjandevries@hotmail.com>
2015-06-13 08:35:17 +0200

    Separated a script for creating a VirtualEnv for machine learning

---
+++ create-VirtualEnvML.sh
@@ -0,0 +1,39 @@
+echo -e "\033[1;33mCreating and activating a temporary VirtualEnv\033[0m"
+ENV_NAME=${1:-env}
+virtualenv $ENV_NAME
+source $ENV_NAME/bin/activate
+
+echo -e ""
+echo -e "\033[1;33mCreating symbolic links for using PyGTK (with matplotlib) in the virtualenv\033[0m"
+ln -sf /usr/lib/python2.7/dist-packages/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} $ENV_NAME/lib/python2.7/site-packages
+
+echo -e ""
+echo -e "\033[1;33mConfiguring SSL for pip in the VirtualEnv\033[0m"
+pip install pyopenssl ndg-httpsclient pyasn1
+
+echo -e ""
+echo -e "\033[1;33mInstalling \033[1;32mnumpy\033[1;33m in the VirtualEnv\033[0m"
+pip install -U numpy
+echo -e ""
+echo -e "\033[1;33mInstalling \033[1;32mpandas\033[1;33m in the VirtualEnv\033[0m"
+pip install -U pandas
+echo -e ""
+echo -e "\033[1;33mInstalling \033[1;32mscipy\033[1;33m in the VirtualEnv\033[0m"
+pip install -U scipy
+echo -e ""
+echo -e "\033[1;33mInstalling \033[1;32mnltk\033[1;33m in the VirtualEnv\033[0m"
+pip install -U nltk
+echo -e ""
+echo -e "\033[1;33mInstalling \033[1;32mscikit-learn\033[1;33m in the VirtualEnv\033[0m"
+pip install -U scikit-learn
+echo -e ""
+echo -e "\033[1;33mInstalling \033[1;32mmatplotlib\033[1;33m in the VirtualEnv\033[0m"
+pip install -U matplotlib
+echo -e ""
+echo -e "\033[1;33mInstalling \033[1;32mTheano\033[1;33m in the VirtualEnv\033[0m"
+pip install -U Theano
+
+echo -e ""
+echo -e "\033[1;33mPip list inside the VirtualEnv\033[0m"
+pip list
+
--- prep-VirtualEnv.sh
+++ prep-VirtualEnv.sh
@@ -3,44 +3,7 @@ echo -e "\033[1;33m\033[40m   Creating a temporary VirtualEnv and             \0
 echo -e "\033[1;33m\033[40m   building Wheels for Machine Learning packages   \033[0m"
 echo -e "\033[1;33m\033[40m                                                   \033[0m"
 
-echo -e ""
-echo -e "\033[1;33mCreating and activating a temporary VirtualEnv\033[0m"
-virtualenv env
-source env/bin/activate
-
-echo -e ""
-echo -e "\033[1;33mCreating symbolic links for using PyGTK (with matplotlib) in the virtualenv\033[0m"
-ln -sf /usr/lib/python2.7/dist-packages/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} env/lib/python2.7/site-packages
-
-echo -e ""
-echo -e "\033[1;33mConfiguring SSL for pip in the VirtualEnv\033[0m"
-pip install pyopenssl ndg-httpsclient pyasn1
-
-echo -e ""
-echo -e "\033[1;33mInstalling \033[1;32mnumpy\033[1;33m in the VirtualEnv\033[0m"
-pip install -U numpy
-echo -e ""
-echo -e "\033[1;33mInstalling \033[1;32mpandas\033[1;33m in the VirtualEnv\033[0m"
-pip install -U pandas
-echo -e ""
-echo -e "\033[1;33mInstalling \033[1;32mscipy\033[1;33m in the VirtualEnv\033[0m"
-pip install -U scipy
-echo -e ""
-echo -e "\033[1;33mInstalling \033[1;32mnltk\033[1;33m in the VirtualEnv\033[0m"
-pip install -U nltk
-echo -e ""
-echo -e "\033[1;33mInstalling \033[1;32mscikit-learn\033[1;33m in the VirtualEnv\033[0m"
-pip install -U scikit-learn
-echo -e ""
-echo -e "\033[1;33mInstalling \033[1;32mmatplotlib\033[1;33m in the VirtualEnv\033[0m"
-pip install -U matplotlib
-echo -e ""
-echo -e "\033[1;33mInstalling \033[1;32mTheano\033[1;33m in the VirtualEnv\033[0m"
-pip install -U Theano
-
-echo -e ""
-echo -e "\033[1;33mPip list inside the VirtualEnv\033[0m"
-pip list
+. create-VirtualEnvML.sh env
 
 echo -e ""
 echo -e "\033[1;33mDeactivating and removing the VirtualEnv\033[0m"
