e701e75f429298376effc402111d60025ffa1319
Erik Jan de Vries <erikjandevries@hotmail.com>
2015-06-27 07:40:21 +0200

    Added installation of Atom text editor

--- prep-atom.sh
+++ prep-atom.sh
@@ -1,6 +1,6 @@
-echo -e "\033[1;33m\033[40m                                     \033[0m"
-echo -e "\033[1;33m\033[40m   Installing the Atom text editor   \033[0m"
-echo -e "\033[1;33m\033[40m                                     \033[0m"
+echo -e "\033[1;33m\033[40m                                          \033[0m"
+echo -e "\033[1;33m\033[40m   Installing the Atom text editor        \033[0m"
+echo -e "\033[1;33m\033[40m                                          \033[0m"
 
-wget -qO- https://atom.io/download/deb > ~/Downloads/atom-amd64.deb
+wget https://atom.io/download/deb > ~/Downloads/atom-amd64.deb
 sudo dpkg -i ~/Downloads/atom-amd64.deb
