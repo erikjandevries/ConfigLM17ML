74c0be59e3f50f30335dc0efa41931d12dd90190
Erik Jan de Vries <erikjandevries@hotmail.com>
2015-07-07 23:33:36 +0200

    Added LibreOffice Base

---
+++ prep-libreoffice.sh
@@ -0,0 +1,10 @@
+echo -e "\033[1;33m\033[40m                                 \033[0m"
+echo -e "\033[1;33m\033[40m   Installing LibreOffice Base   \033[0m"
+echo -e "\033[1;33m\033[40m                                 \033[0m"
+
+sudo apt-get install -y libreoffice-base libreoffice-sdbc-postgresql
+
+echo -e "\033[1;33m\033[40m                                          \033[0m"
+echo -e "\033[1;33m\033[40m   Finished installing LibreOffice Base   \033[0m"
+echo -e "\033[1;33m\033[40m                                          \033[0m"
+echo -e ""
