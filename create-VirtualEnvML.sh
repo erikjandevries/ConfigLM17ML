86f5bca02929cd23c99ab733b9f4a5fda5053455
Erik Jan de Vries <erikjandevries@hotmail.com>
2015-11-02 15:46:02 +0100

    Removed installation of basemap from general virtualenv creation.

--- create-VirtualEnvML.sh
+++ create-VirtualEnvML.sh
@@ -44,7 +44,7 @@ echo -e ""
 echo -e "\033[1;33mInstalling \033[1;32mpsycopg2\033[1;33m in the VirtualEnv\033[0m"
 pip install -U psycopg2
 
-. prep-basemap.sh
+# . prep-basemap.sh
 
 echo -e ""
 echo -e "\033[1;33mPip list inside the VirtualEnv\033[0m"
