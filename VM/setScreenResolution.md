9a4247b218dfb8fd932a82d40547ffeadd79c991
Erik Jan de Vries <erikjandevries@hotmail.com>
2015-11-10 16:26:47 +0100

    Added description for setting screen resolution

---
+++ VM/setScreenResolution.md
@@ -0,0 +1,13 @@
+# Set Screen Resolution
+
+If by default the 1920x1080 screen resolution is not enabled, you can use the shell script
+
+> setScreenResolution.sh
+
+to automatically set the screen resolution at login.
+
+Copy the file to your home directory, set the execution permissions:
+
+> chmod 755 setScreenResolution.sh
+
+and add the script to the Startup Applications.
--- prep-private.sh
+++ prep-private.sh
@@ -1,5 +1,14 @@
+echo -e "\033[1;33m\033[40m                                               \033[0m"
+echo -e "\033[1;33m\033[40m   Executing private script(s)                 \033[0m"
+echo -e "\033[1;33m\033[40m                                               \033[0m"
+
 mkdir -p private
 touch ./private/prep-private.sh
 cd private
 . prep-private.sh
 cd ..
+
+echo -e "\033[1;33m\033[40m                                               \033[0m"
+echo -e "\033[1;33m\033[40m   Finished executing private script(s)        \033[0m"
+echo -e "\033[1;33m\033[40m                                               \033[0m"
+
