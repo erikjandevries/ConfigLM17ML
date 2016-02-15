14efb40cc583098c60fd8532abdef32aedefe2db
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-02-15 11:13:30 +0100

    Added Atom packages to install script

--- prep-atom.sh
+++ prep-atom.sh
@@ -4,3 +4,12 @@ echo -e "\033[1;33m\033[40m                                          \033[0m"
 
 wget -O ~/Downloads/atom-amd64.deb https://atom.io/download/deb
 sudo dpkg -i ~/Downloads/atom-amd64.deb
+
+apm install atomatigit
+# apm install language-diff
+apm install language-matlab
+apm install language-r
+apm install merge-conflicts
+apm install split-diff
+
+apm upgrade --no-confirm
